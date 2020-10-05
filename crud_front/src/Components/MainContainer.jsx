7e24d51bb7e721841d5c8b757ffac2ff03c59fd3
ataruby <atarubift@gmail.com>
2020-10-05 16:18:10 +0900

    write Ajax

--- crud_front/src/Components/MainContainer.jsx
+++ crud_front/src/Components/MainContainer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import axios from 'axios'
 import ProductsContainer from './ProductsContainer'
 import FormContainer from './FormContainer'
-import { FormControl } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css'
+import update from 'react-addons-update'
 
 class MainContainer extends React.Component {
     constructor(props) {
@@ -23,10 +25,21 @@ class MainContainer extends React.Component {
         })
     }
 
+    createProduct = (product) => {
+        axios.post('http://localhost:3001/products',{product: product} )
+        .then((response) => {
+            const newData = update(this.state.products,{$push:[response.data]})
+            this.setState({products: newData})
+        })
+        .catch((data) => {
+            console.log(data)
+        })
+    }
+
     render() {
         return (
             <div className='app-main'>
-                <FormContainer />
+                <FormContainer createProduct={this.createProduct} />
                 <ProductsContainer productData={ this.state.products } />
             </div>
         );
