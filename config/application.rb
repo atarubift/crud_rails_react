561222e2c0eff788dc75fb652bad2199217c753b
ataruby <atarubift@gmail.com>
2020-10-05 13:00:19 +0900

    fix App.js

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module CrudApp
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'http://localhost:3000'
-        resources '*',
+        resource '*',
         :headers => :any,
         :methods => [:get, :post, :patch, :delete, :options]
       end
