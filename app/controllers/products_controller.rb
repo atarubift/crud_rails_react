12e46c6f14c7fdea258700fe3877dbcc53cc395e
ataruby <atarubift@gmail.com>
2020-10-05 11:52:46 +0900

    write produsts_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,28 @@
 class ProductsController < ApplicationController
+
+    def index
+        @product = Product.all
+        rebder json: @product
+    end
+
+    def create
+        @product = Product.create(product: params[:product])
+        render json: @product
+    end
+
+    def update
+        @product = Product.find(params[:id])
+        @product.update_attribute(product: params[product])
+        render json: @product
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        if @product.destroy
+            head :no_content, status: :ok
+        else
+            render json: @product.errors, status: :upprocessable_entity
+        end
+    end
+    
 end
